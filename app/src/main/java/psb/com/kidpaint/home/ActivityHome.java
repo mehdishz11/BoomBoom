14c84d93cf950ecaae55a6a204fba4b8e62a1ccd
Nmorteza <morteza.infoapp@gmail.com>
2018-07-02 10:15:59 +0430

    صفحه هوم

---
+++ app/src/main/java/psb/com/kidpaint/home/ActivityHome.java
@@ -0,0 +1,16 @@
+package psb.com.kidpaint.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import psb.com.kidpaint.R;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        String a="ghfdjgd";
+    }
+}
