3c0778bf3a3dd622d70ba8b249321a736ced4eae
Nmorteza <morteza.infoapp@gmail.com>
2018-07-02 16:20:16 +0430

    صفحه هوم

--- app/src/main/java/psb/com/kidpaint/home/ActivityHome.java
+++ app/src/main/java/psb/com/kidpaint/home/ActivityHome.java
@@ -1,16 +1,26 @@
 package psb.com.kidpaint.home;
 
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import psb.com.kidpaint.R;
+import psb.com.kidpaint.home.history.FragmentHistory;
 
-public class ActivityHome extends AppCompatActivity {
+public class ActivityHome extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener,FragmentHistory.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        String a="ghfdjgd";
+      /*  AppBarLayout appBar = findViewById(R.id.app_bar);
+        appBar.addOnOffsetChangedListener(this);*/
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new FragmentHistory()).commit();
+
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
     }
 }
